333ee386410cbec677496f76db0a319941d92fa5
Kardian <kuszawet@gmail.com>
2022-10-25 14:32:11 +0200

    adding color icons + functions changing color

--- src/components/StockList.js
+++ src/components/StockList.js
@@ -1,14 +1,22 @@
 import { useState, useEffect } from "react"
+import { BsFillCaretDownFill, BsFillCaretUpFill } from "react-icons/bs";
 import finnHub from "../apis/finnHub"
 
 export const StockList = () => {
     const [stock, setStock] = useState([])
     const [watchList, setWatchList] = useState(["GOOGL", "MSFT", "AMZN"])
 
+    const changeColor = (change) => {
+        return change > 0 ? "success": "danger"
+    }
+
+    const renderIcon = (change) => {
+        return change > 0 ? <BsFillCaretUpFill /> : <BsFillCaretDownFill />
+    }
+
     useEffect(() => {
         let isMounted = true
         const fetchData = async () => {
-            const responses = []
             try {
                 const responses = await Promise.all(watchList.map((stock) => {
                     return finnHub.get("/quote", {
@@ -53,10 +61,10 @@ export const StockList = () => {
                 {stock.map((stockData) => {
                     return (
                         <tr className="table-row" key={stockData.symbol}>
-                            <th scope="row">{stockData}.symbol</th>
+                            <th scope="row">{stockData.symbol}.symbol</th>
                             <td>{stockData.data.c}</td>
-                            <td>{stockData.data.d}</td>
-                            <td>{stockData.data.dp}</td>
+                            <td className={`text-${changeColor(stockData.data.d)}`}>{stockData.data.d}{renderIcon(stockData.data.d)}</td>
+                            <td className={`text-${changeColor(stockData.data.d)}`}>{stockData.data.dp}{renderIcon(stockData.data.d)}</td>
                             <td>{stockData.data.h}</td>
                             <td>{stockData.data.l}</td>
                             <td>{stockData.data.o}</td>
