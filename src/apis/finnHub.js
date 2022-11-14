f74c013893a9fb4ba0da03f476d8298ddf2402a5
Kardian <kuszawet@gmail.com>
2022-11-15 00:58:38 +0100

    charts logic and all

--- src/apis/finnHub.js
+++ src/apis/finnHub.js
@@ -1,10 +1,10 @@
-import axios from "axios";
+import axios from "axios"
 
-const TOKEN = "cc26pu2ad3icrd1@orgg"
+const TOKEN = "cc26pu2ad3icrd10orqg"
 
 export default axios.create({
-    baseURL: "https://finnhub.io/api/v1",
-    params: {
-        token: TOKEN
-    }
+  baseURL: "https://finnhub.io/api/v1",
+  params: {
+    token: TOKEN
+  }
 })
